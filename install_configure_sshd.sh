c89e74fd858b9e95efd80f9de67d25083e6f74f9
Arthur Burkart <artburkart@gmail.com>
2018-06-22 17:31:08 -0400

    Make regex more explicit

--- install_configure_sshd.sh
+++ install_configure_sshd.sh
@@ -3,7 +3,7 @@
 if grep -q '#AuthorizedKeysCommand none' "$SSHD_CONFIG_FILE"; then
   sed -i "s:#AuthorizedKeysCommand none:AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}:g" "$SSHD_CONFIG_FILE"
 else
-  if ! grep -q "AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}" "$SSHD_CONFIG_FILE"; then
+  if ! grep -q "^AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}" "$SSHD_CONFIG_FILE"; then
     echo "AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}" >> "$SSHD_CONFIG_FILE"
   fi
 fi
@@ -12,7 +12,7 @@ if grep -aq 'AuthorizedKeysCommandUser' "$SSHD_CONFIG_FILE"; then
   if grep -q '#AuthorizedKeysCommandUser nobody' "$SSHD_CONFIG_FILE"; then
     sed -i "s:#AuthorizedKeysCommandUser nobody:AuthorizedKeysCommandUser nobody:g" "$SSHD_CONFIG_FILE"
   else
-    if ! grep -q 'AuthorizedKeysCommandUser nobody' "$SSHD_CONFIG_FILE"; then
+    if ! grep -q '^AuthorizedKeysCommandUser nobody' "$SSHD_CONFIG_FILE"; then
       echo "AuthorizedKeysCommandUser nobody" >> "$SSHD_CONFIG_FILE"
     fi
   fi
