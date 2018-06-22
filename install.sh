fc17a49b5880b8420e8fd7c7d5c09da09e6af0f2
Arthur Burkart <artburkart@gmail.com>
2018-06-22 15:27:30 -0400

    Uses simplisafe repo until PR is merged

--- install.sh
+++ install.sh
@@ -97,7 +97,7 @@ tmpdir=$(mktemp -d)
 
 cd "$tmpdir"
 
-git clone -b master https://github.com/widdix/aws-ec2-ssh.git
+git clone -b simplisafe/centos6-support https://github.com/simplisafe/aws-ec2-ssh.git
 
 cd "$tmpdir/aws-ec2-ssh"
 
